90022e9745c3099aed5242462b6fd79593a988e1
khushi003-arch <khushivishwakarma9016@gmail.com>
2025-12-07 13:49:09 +0530

    initial commit

---
+++ chapter1/first.py
@@ -0,0 +1,18 @@
+print("hello wrold from python language. and khushi learn this definitialy. ")
+
+
+# print your name 3 Times uisng * symbol and each line should be on next line :
+
+print(("khushi vishvakarma" + "\n")*3)
+
+
+
+# print name and age in same line :
+
+print("My Name is Khushi","My Age is 20:","\n")
+
+
+# print the number 23. and also sum of this number by twice:
+
+print(23)
+print(23+23)
