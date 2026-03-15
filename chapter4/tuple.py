278d7fb9a370c5d1c208e71283010d00da40bf34
khushi003-arch <khushivishwakarma9016@gmail.com>
2026-03-16 00:04:12 +0530

    tuples are done

--- chapter4/tuple.py
+++ chapter4/tuple.py
@@ -21,3 +21,60 @@ print(type(tup2))
 tup=()
 print(type(tup))
 
+
+
+# slicing
+
+print("slicing",my_tuple[0:3])
+
+
+# methods :
+
+# index() return the index numbers of the mentioned items in parameter on first occurrence.
+print("index",my_tuple.index('sunday'))
+
+
+# methods :
+
+# count() return the total occuerence of the items mentioned items in parameter 
+print("count",my_tuple.count('sunday'))
+
+
+
+
+# ============================  practice questions ================================
+
+
+# empty_list=[]
+
+# for i in range(0 ,3):
+#     user_input=input("Enter Your MOvie Name :")
+#     empty_list.append(user_input)
+
+# print(empty_list)
+
+
+
+
+# Task 2.
+
+palindrom_list=['abc','efe','khushi','123','roshni']
+
+for i in palindrom_list:
+    if i==i[::-1] :
+        print(i,"is palindrome")
+    else :
+        print(i,"is not palindrome")
+
+
+
+# Task 3.
+
+tuple=('a','c','a','b','b','v','p')
+print("Count",tuple.count('b'))
+
+list=['a','c','a','b','b','v','p']
+list.sort(reverse=True)
+print(list)
+
+
